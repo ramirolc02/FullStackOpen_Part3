c85a0f611664a648f6ad3faf97598e0e58b67d6d
Rama Lopez Cento <ramirolopezcento@icloud.com>
2022-09-23 20:52:15 +0200

    3.8 done

--- Phonebook_backend/index.js
+++ Phonebook_backend/index.js
@@ -1,10 +1,21 @@
 
 const express = require('express')
-const morgan = require('morgan')
+ const morgan = require('morgan')
 
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+ // app.use(morgan('tiny'))
+
+ morgan.token('content', (req,res) => {
+  if (req.method === "POST") {
+    return JSON.stringify(req.body) 
+  }
+    else{
+      return " ";
+    }
+  })
+  
+  app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 let persons = [
   { 
