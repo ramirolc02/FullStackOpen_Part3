638e8c48556eab4bf08bbbb792b3349a6a0fb579
Rama Lopez Cento <ramirolopezcento@icloud.com>
2022-11-04 23:18:32 +0100

    uibuild

--- Phonebook_backend/index.js
+++ Phonebook_backend/index.js
@@ -1,9 +1,9 @@
-
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
-require('dotenv').config()
+
 const Person = require('./models/person')
 
 app.use(express.static('build'))
